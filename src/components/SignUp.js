b2b364d589f76a2704b2d48b08dbea62dbd9b768
samie820 <samuelomole70@gmail.com>
2018-02-10 16:24:34 +0100

    Added the navigation

---
+++ src/components/SignUp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Constants } from 'expo';
+
+
+
+class SignUp extends React.Component {
+    
+    static navigationOptions = {
+      title: 'Recent',
+    }
+
+    render()
+     {
+      return (
+        <Text>List of recent chats </Text>
+      );
+      
+         
+        
+    }
+  }
+export default SignUp;
